9f04dee8d2b58bee6a2f9776bed5ee86bc71b465
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-06-01 18:29:21 +0200

    #2 header html toegevoegd

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log('Hier komt je server voor Sprint 12.')
+
+
+
+import express from "express";
+
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from "liquidjs";
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express();
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({ extended: true }));
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static("public"));
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set("views", "./views");
+
+
+app.get("/test", async function (request, response) {
+
+    response.render("index.liquid", { });
+});
