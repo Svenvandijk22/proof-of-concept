5ac3089143469e66d1bfc49f888e60420ecf9c65
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-06-04 17:03:51 +0200

    #5 variabel aangemaakt waar ik de thumbnail en main image toevoeg

---
+++ public/client.js
@@ -0,0 +1,6 @@
+// Stap 1: Pak alle thumbnails uit de HTML
+const thumbnails = document.querySelectorAll(".gallery-thumbnail");
+
+// Stap 2: Pak de grote hoofdafbeelding uit de HTML
+const mainImage = document.querySelector(".gallery-main-image");
+
